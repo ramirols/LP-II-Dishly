ae1a5143a5acdacbc6949c52e673e746174ddcaf
ramirols <ramirols.dev@gmail.com>
2026-02-22 14:22:11 -0500

    :tada: arreglo en CarritoItem

--- LP II - Dishly Proyecto Final/dishly/src/main/java/com/dishly/app/model/CarritoItem.java
+++ LP II - Dishly Proyecto Final/dishly/src/main/java/com/dishly/app/model/CarritoItem.java
@@ -2,17 +2,13 @@ package com.dishly.app.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
 public class CarritoItem {
-	private Plato plato;
+    private Plato plato;
     private int cantidad;
 
     public double getSubtotal() {
