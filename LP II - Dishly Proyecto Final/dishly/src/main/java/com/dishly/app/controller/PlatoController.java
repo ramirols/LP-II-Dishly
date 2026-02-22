8811b2d46da3411b4691d77c8fdeb241554016ab
David Carmona <davidc.carmonac@gmail.com>
2026-02-21 20:11:13 -0500

    logica de gestion de categorias

--- LP II - Dishly Proyecto Final/dishly/src/main/java/com/dishly/app/controller/PlatoController.java
+++ LP II - Dishly Proyecto Final/dishly/src/main/java/com/dishly/app/controller/PlatoController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dishly.app.model.Categoria;
 import com.dishly.app.model.Plato;
 import com.dishly.app.service.CategoriaService;
 import com.dishly.app.service.PlatoService;
 
+import dto.CategoriaDTO;
 import dto.PlatoDTO;
 
 @Controller
@@ -26,6 +28,7 @@ public class PlatoController {
 	public String gestionarPlatos(Model model) {
 		
 		model.addAttribute("platoDTO", new PlatoDTO());
+		model.addAttribute("categoriaDTO", new CategoriaDTO());
 		model.addAttribute("platos", platoService.listarTodo());
 		model.addAttribute("categorias", categoriaService.listarTodo());
 		
@@ -51,12 +54,37 @@ public class PlatoController {
 		plato.setEstado(true);
 		platoService.guardarPlato(plato);
 		
-		return "redirect:/admin/platos/inicio?registroExitoso";
+		return "redirect:/admin/platos/inicio?platoGuardado";
 	}
 	
 	@PostMapping("/eliminar")
 	public String eliminarPlato(@ModelAttribute PlatoDTO platoDTO) {
 		platoService.eliminarPlatoPorId(platoDTO.getId());
 		return "redirect:/admin/platos/inicio?platoEliminado";
+	}
+	
+	@PostMapping("/categoria/guardar")
+	public String guardarCategoria(@ModelAttribute CategoriaDTO categoriaDTO) {
+		
+		Categoria categoria;
+		
+		if (categoriaDTO.getId() != null) {
+			categoria = categoriaService.buscarCategoriaPorId(categoriaDTO.getId());
+		} else {
+			categoria = new Categoria();
+		}
+		
+		categoria.setNombre(categoriaDTO.getNombre());
+		categoria.setDescripcion(categoriaDTO.getDescripcion());
+		
+		categoriaService.guardarCategoria(categoria);
+		
+		return "redirect:/admin/platos/inicio?categoriaAgregada";
+	}
+	
+	@PostMapping("/categoria/eliminar")
+	public String eliminarCategoria(@ModelAttribute CategoriaDTO categoriaDTO) {
+		categoriaService.eliminarCategoriaPorId(categoriaDTO.getId());
+		return "redirect:/admin/platos/inicio?categoriaEliminada";
 	}
 }
